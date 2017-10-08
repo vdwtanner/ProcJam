860e370a841d39c3b478492958c9b19a5f52f2ae
vdwtanner <vdwtanner@gmail.com>
2017-10-08 17:36:21 -0500

    Added a meh script that will automatically rotate an object about the world up axis in the editor so that Jun can see his models under different lighting angles

---
+++ ProcJam/Assets/Scripts/Tools/LightingTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class LightingTest : MonoBehaviour {
+	public float speed = 5;
+	public bool pause = true;
+	public bool reset = false;
+	void Awake()
+	{
+		runInEditMode = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!pause)
+		{
+			transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+
+		}
+		if (reset)
+		{
+			pause = true;
+			transform.rotation = Quaternion.Euler(35, 0, 0);
+			reset = false;
+		}
+	}
+}
